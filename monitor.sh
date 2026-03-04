14ff8e99d5f2703c0ffac61d6369898f1486b962
Grish-Khechoyan <143726992+Grish-Khechoyan@users.noreply.github.com>
2026-03-04 17:42:32 +0000

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ##-> Print the last field, wich shows the amount  of available RAM(MB).
 SYSTEM_STATS="System Check: Available RAM is $(free -m | awk '/Mem:/ {print $7 " MB"}')" 
 echo $SYSTEM_STATS
